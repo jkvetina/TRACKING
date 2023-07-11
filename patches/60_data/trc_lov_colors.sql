62a012ca06fada42d8e8a3ab418a37b78032f186
Jan Kvetina <jan.kvetina@gmail.com>
2023-07-11 14:04:54 +0200

    Patches

---
+++ patches/60_data/trc_lov_colors.sql
@@ -0,0 +1,53 @@
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('--');
+    DBMS_OUTPUT.PUT_LINE('-- MERGE ' || UPPER('trc_lov_colors'));
+    DBMS_OUTPUT.PUT_LINE('--');
+END;
+/
+--
+DELETE FROM trc_lov_colors;
+--
+MERGE INTO trc_lov_colors t
+USING (
+    SELECT 'COLOR_ACTIVITY' AS name, 0 AS treshold, '#508104' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_ACTIVITY' AS name, 10 AS treshold, '#9e8e01' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_ACTIVITY' AS name, 20 AS treshold, '#f3b800' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_ACTIVITY' AS name, 50 AS treshold, '#db8200' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_ACTIVITY' AS name, 100 AS treshold, '#b64201' AS color_bg, '#fff' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_AVG_TIME' AS name, 0 AS treshold, '#508104' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_AVG_TIME' AS name, 0.5 AS treshold, '#9e8e01' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_AVG_TIME' AS name, 1 AS treshold, '#f3b800' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_AVG_TIME' AS name, 1.5 AS treshold, '#db8200' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_AVG_TIME' AS name, 2 AS treshold, '#b64201' AS color_bg, '#fff' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_MAX_TIME' AS name, 0 AS treshold, '#508104' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_MAX_TIME' AS name, 0.5 AS treshold, '#9e8e01' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_MAX_TIME' AS name, 1 AS treshold, '#f3b800' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_MAX_TIME' AS name, 1.5 AS treshold, '#db8200' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_MAX_TIME' AS name, 2 AS treshold, '#b64201' AS color_bg, '#fff' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_USERS' AS name, 0 AS treshold, '#508104' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_USERS' AS name, 5 AS treshold, '#9e8e01' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_USERS' AS name, 10 AS treshold, '#f3b800' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_USERS' AS name, 20 AS treshold, '#db8200' AS color_bg, '#000' AS color_text FROM DUAL UNION ALL
+    SELECT 'COLOR_USERS' AS name, 50 AS treshold, '#b64201' AS color_bg, '#fff' AS color_text FROM DUAL
+) s
+ON (
+    t.name = s.name
+    AND t.treshold = s.treshold
+)
+--WHEN MATCHED THEN
+--    UPDATE SET
+--        t.color_bg = s.color_bg,
+--        t.color_text = s.color_text
+WHEN NOT MATCHED THEN
+    INSERT (
+        t.name,
+        t.treshold,
+        t.color_bg,
+        t.color_text
+    )
+    VALUES (
+        s.name,
+        s.treshold,
+        s.color_bg,
+        s.color_text
+    );
